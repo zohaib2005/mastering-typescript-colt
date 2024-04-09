ceb57d08f4b5ecccefa94b8e1f107d48d3ac6121
zohaib <zohaibshafiq2005@gmail.com>
2024-04-09 12:57:24 +0500

    defining an interface

--- 06 Interfaces.ts
+++ 06 Interfaces.ts
@@ -13,3 +13,22 @@ interface Point {
 }
 
 const pt: Point = { x: 123, y: 1234 };
+
+interface Person {
+  readonly id: number;
+  first: string;
+  last: string;
+  nickname?: string;
+  // sayHi: () => string;
+  sayHi(): string;
+}
+
+const thomas: Person = {
+  first: "Thomas",
+  last: "Hardy",
+  nickname: "Tom",
+  id: 21837,
+  sayHi: () => {
+    return "Hello!";
+  },
+};
