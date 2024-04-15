a4e05f0f0a4d157501e071ddf6d0a37f28a1623d
zohaib <zohaibshafiq2005@gmail.com>
2024-04-15 12:57:45 +0500

    providing a type to querySelector

--- 08 generics.ts
+++ 08 generics.ts
@@ -1,1 +1,6 @@
+// Providing a type to querySelector:
+const inputEl = document.querySelector<HTMLInputElement>("#username")!;
+console.dir(inputEl);
+inputEl.value = "Hacked!";
 
+const btn = document.querySelector<HTMLButtonElement>(".btn")!;
