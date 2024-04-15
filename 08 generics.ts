9feff479756a328eeb57ca9a6ee579720f394caa
zohaib <zohaibshafiq2005@gmail.com>
2024-04-15 20:51:23 +0500

    basic identity generics

--- 08 generics.ts
+++ 08 generics.ts
@@ -4,3 +4,14 @@ console.dir(inputEl);
 inputEl.value = "Hacked!";
 
 const btn = document.querySelector<HTMLButtonElement>(".btn")!;
+
+// Without Generics...Lots of Repetition!
+function numberIdentity(item: number): number {
+  return item;
+}
+function stringIdentity(item: string): string {
+  return item;
+}
+function booleanIdentity(item: boolean): boolean {
+  return item;
+}
