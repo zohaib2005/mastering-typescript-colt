2ae513aea0f64ca8015249440cc099caa291490e
zohaib <zohaibshafiq2005@gmail.com>
2024-04-16 10:22:34 +0500

    reusable generic

--- 08 generics.ts
+++ 08 generics.ts
@@ -15,3 +15,24 @@ function stringIdentity(item: string): string {
 function booleanIdentity(item: boolean): boolean {
   return item;
 }
+
+// function identity(item: any): any{
+//   return item;
+// }
+
+// With A Generic...Super Reusable!
+function identity<T>(item: T): T {
+  return item;
+}
+
+identity<number>(7);
+identity<string>("hello");
+
+function getRandomElement<T>(list: T[]): T {
+  const randIdx = Math.floor(Math.random() * list.length);
+  return list[randIdx];
+}
+
+console.log(getRandomElement<string>(["a", "b", "c"]));
+getRandomElement<number>([5, 6, 21, 354, 567, 234, 654]);
+getRandomElement([1, 2, 3, 4]);
