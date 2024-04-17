30547ac882261ef9237b684a65b30f24c8175714
zohaib <zohaibshafiq2005@gmail.com>
2024-04-17 16:11:27 +0500

    truthiness narrowing

--- 09 TypeNarrowing.ts
+++ 09 TypeNarrowing.ts
@@ -5,3 +5,21 @@ function triple(value: number | string) {
   }
   return value * 3;
 }
+
+const el = document.getElementById("idk");
+if (el) {
+  el;
+} else {
+  el;
+}
+
+// Truthiness Narrowing:
+const printLetters = (word?: string) => {
+  if (word) {
+    for (let char of word) {
+      console.log(char);
+    }
+  } else {
+    console.log("YOU DID NOT PASS IN A WORD!");
+  }
+};
