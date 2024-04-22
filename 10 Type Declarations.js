6dab84a37336fc1f9966a97351d56b1733e4889c
zohaib <zohaibshafiq2005@gmail.com>
2024-04-22 23:54:19 +0500

    working with axios types

--- 10 Type Declarations.js
+++ 10 Type Declarations.js
@@ -1,12 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
+// axios
+//   .get<User>("https://jsonplaceholder.typicode.com/users/1")
+//   .then((res) => {
+//     console.log("WOOO!!!");
+//     printUser(res.data);
+//   })
+//   .catch((e) => {
+//     console.log("ERROR!", e);
+//   });
 axios_1.default
-    .get("https://jsonplaceholder.typicode.com/users/1")
+    .get("https://jsonplaceholder.typicode.com/users")
     .then(function (res) {
     console.log("WOOO!!!");
-    console.log(res.data);
+    res.data.forEach(printUser);
 })
     .catch(function (e) {
     console.log("ERROR!", e);
 });
+function printUser(user) {
+    console.log(user.name);
+    console.log(user.email);
+    console.log(user.phone);
+}
--- 10 Type Declarations.ts
+++ 10 Type Declarations.ts
@@ -1,11 +1,51 @@
 import axios from "axios";
 
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+// axios
+//   .get<User>("https://jsonplaceholder.typicode.com/users/1")
+//   .then((res) => {
+//     console.log("WOOO!!!");
+//     printUser(res.data);
+//   })
+//   .catch((e) => {
+//     console.log("ERROR!", e);
+//   });
+
 axios
-  .get("https://jsonplaceholder.typicode.com/users/1")
+  .get<User[]>("https://jsonplaceholder.typicode.com/users")
   .then((res) => {
     console.log("WOOO!!!");
-    console.log(res.data);
+    res.data.forEach(printUser);
   })
   .catch((e) => {
     console.log("ERROR!", e);
   });
+
+function printUser(user: User) {
+  console.log(user.name);
+  console.log(user.email);
+  console.log(user.phone);
+}
