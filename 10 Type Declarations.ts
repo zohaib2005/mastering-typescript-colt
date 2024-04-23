4d54d6285d53a4f05d87092acd1c80b374871f30
zohaib <zohaibshafiq2005@gmail.com>
2024-04-23 08:08:59 +0500

    installing 3rd party types seperately

--- 10 Type Declarations.ts
+++ 10 Type Declarations.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import _ from "lodash";
+
+_.sample(234);
 
 interface User {
   id: number;
@@ -49,3 +52,15 @@ function printUser(user: User) {
   console.log(user.email);
   console.log(user.phone);
 }
+
+// INSTALLING TYPES SEPERATELY --->
+
+// TypeScript comes with some build in 3rd party npm packages types like axios but for some like lodash we need to install types ourself, so there is a huge project on github
+// named github.com/DefinitelyTyped/DefinitelyTyped
+// On this github repo project almost 99.99% of 3rd party packages types are available which you can install
+// i.e npm install --save-dev @types/node
+// install any library from npm then
+// install its types from npm install --save-dev @types/packagename
+// we install it only in dev dependency because we don't need on production we only need on development
+// We can also search available 3rd party npm package types on Typescript.org
+// typescriptlang.org/dt/search?search=
